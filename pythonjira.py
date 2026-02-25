56fe4bfecd0e3b2b04fa406a7025ae3c5f12f7f4
ssn5286 <ssn5286@yahoo.com>
2026-02-24 19:50:39 -0500

    add python jira-1

---
+++ pythonjira.py
@@ -0,0 +1,27 @@
+# http://jira.readthedocs.org/en/latest/#issues
+
+from jira import JIRA
+jiraOptions = {'server': "https://s-s-n.atlassian.net"}
+jira = JIRA(options=jiraOptions, basic_auth=("ssn5286@yahoo.com", "")) 
+
+for project in jira.projects(): print(project.key)
+
+# for comments in jira.comment(): print(comments.key)
+jql_query = 'project = "KAN" AND status = "To Do"'
+# Set maxResults=False to fetch all issues, automatically handling pagination
+issues = jira.search_issues(jql_query, maxResults=False)
+# Loop through issues
+for issue in issues:
+    print(f"{issue.key}: {issue.fields.summary}")
+    comments = issue.fields.comment.comments
+    for comment in comments:
+            print(f"Comment ID: {comment.id}")
+            print(f"Author: {comment.author.displayName}")
+            print(f"Created: {comment.created}")
+            print(f"Body: {comment.body}\n")
+
+for singleIssue in jira.search_issues(jql_str='project = KAN'): 
+    print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary, singleIssue.fields.reporter.displayName))
+
+
+
